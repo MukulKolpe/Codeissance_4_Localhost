b0aff44b5350a21618bc87f30e2ac9447fc2dc40
Mukul Kolpe <mukulkolpe45@gmail.com>
2022-09-23 18:09:55 +0530

    Added footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home/Home";
 import UploadFile from "./Pages/UploadFile/UploadFile";
 import Navbar from "./Components/Navbar/Navbar";
+import Footer from "./Components/Footer/Footer";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/upload" element={<UploadFile />} />
         </Routes>
+        <Footer />
       </Router>
     </div>
   );
---
+++ frontend/src/Components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <div className="main-footer">
+      <div className="container">Made with ❤️ by Localhost</div>
+    </div>
+  );
+}
+
+export default Footer;
