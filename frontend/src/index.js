8119d2fdb247e6d9bd2eb99046329772ceca0b51
SarveshLimaye <sarveshlimaye2002@gmail.com>
2022-09-23 18:27:44 +0530

    Moralis upload file done

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { MoralisProvider } from "react-moralis";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <MoralisProvider appId={process.env.REACT_APP_appId} serverUrl={process.env.REACT_APP_serverUrl}>
   <React.StrictMode>
     <App />
   </React.StrictMode>
+  </MoralisProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
